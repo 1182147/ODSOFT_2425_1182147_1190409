3efe1dac8567f288ca8ef9c1d121fa157e0ab896
António Guerra <1190409@isep.ipp.pt>
2024-11-03 01:51:09 +0000

    test: added transparent box texts for book model

--- src/test/java/pt/psoft/g1/psoftg1/bookmanagement/model/BookTest.java
+++ src/test/java/pt/psoft/g1/psoftg1/bookmanagement/model/BookTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 import pt.psoft.g1.psoftg1.authormanagement.model.Author;
+import pt.psoft.g1.psoftg1.bookmanagement.services.UpdateBookRequest;
 import pt.psoft.g1.psoftg1.genremanagement.model.Genre;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.*;
 
 class BookTest {
     private final String validIsbn = "9782826012092";
@@ -98,6 +100,120 @@ class BookTest {
 
             assertEquals(IllegalArgumentException.class, exception.getClass());
             assertEquals("Author list is empty", exception.getMessage());
+        }
+    }
+
+    @Test
+    void ensureApplyPatchHasExpectedBehavior_WhenNotChangingValidParameters() {
+        try (MockedConstruction<Title> mockedTitle = mockConstruction(Title.class);
+             MockedConstruction<Isbn> mockedIsbn = mockConstruction(Isbn.class);
+             MockedConstruction<Description> mockedDescription = mockConstruction(Description.class)) {
+
+            String expectedIsbn = "9782826012092";
+            String expectedTitle = "Encantos de contar";
+            String expectedDescription = "Descrição";
+            Genre doubleGenre = mock(Genre.class);
+            Author expectedAuthor = new Author("Pedro", "Pedro escritor português", null);
+            List<Author> expectedAuthorList = new ArrayList<>();
+            expectedAuthorList.add(expectedAuthor);
+            String expectedPhotoUri = "photo.jpeg";
+
+            UpdateBookRequest doubleRequest = mock(UpdateBookRequest.class);
+            Book book =
+                    new Book(
+                            expectedIsbn,
+                            expectedTitle,
+                            expectedDescription,
+                            doubleGenre,
+                            expectedAuthorList,
+                            expectedPhotoUri
+                    );
+            book.setVersion(1L);
+
+            Isbn mockIsbn = mockedIsbn.constructed().getFirst();
+            when(mockIsbn.toString()).thenReturn(expectedIsbn);
+
+            Title mockTitle = mockedTitle.constructed().getFirst();
+            when(mockTitle.toString()).thenReturn(expectedTitle);
+
+            when(doubleGenre.toString()).thenReturn("Fantasia");
+
+            Description mockDescription = mockedDescription.constructed().getFirst();
+            when(mockDescription.toString()).thenReturn(expectedDescription);
+
+            when(doubleRequest.getAuthorObjList()).thenReturn(null);
+
+            book.applyPatch(1L, doubleRequest);
+
+            verify(doubleRequest).getTitle();
+            verify(doubleRequest).getDescription();
+            verify(doubleRequest).getGenreObj();
+            verify(doubleRequest).getAuthorObjList();
+            verify(doubleRequest).getPhotoURI();
+
+            assertEquals(doubleGenre, book.getGenre());
+            assertEquals(expectedIsbn, book.getIsbn());
+            assertEquals(expectedTitle, book.getTitle().toString());
+            assertEquals(expectedAuthorList, book.getAuthors());
+            assertEquals(expectedDescription, book.getDescription());
+            assertEquals(expectedPhotoUri, book.getPhoto().getPhotoFile());
+        }
+    }
+
+    @Test
+    void ensureApplyPatchHasExpectedBehavior_WhenChangingValidParameters() {
+        try (MockedConstruction<Title> mockedTitle = mockConstruction(Title.class);
+             MockedConstruction<Isbn> mockedIsbn = mockConstruction(Isbn.class);
+             MockedConstruction<Description> mockedDescription = mockConstruction(Description.class)) {
+
+            String expectedIsbn = "9782826012092";
+            String expectedTitle = "Encantos de contar";
+            String expectedDescription = "Descrição";
+            Genre doubleGenre = mock(Genre.class);
+            Author expectedAuthor = new Author("Pedro", "Pedro escritor português", null);
+            List<Author> expectedAuthorList = new ArrayList<>();
+            expectedAuthorList.add(expectedAuthor);
+            String expectedPhotoUri = "null";
+
+            UpdateBookRequest doubleRequest = mock(UpdateBookRequest.class);
+            Book book =
+                    new Book(
+                            null,
+                            null,
+                            "",
+                            doubleGenre,
+                            expectedAuthorList,
+                            "null"
+                    );
+            book.setVersion(1L);
+
+            Isbn mockIsbn = mockedIsbn.constructed().getFirst();
+            when(mockIsbn.toString()).thenReturn(expectedIsbn);
+
+            Title mockTitle = mockedTitle.constructed().getFirst();
+            when(mockTitle.toString()).thenReturn(expectedTitle);
+
+            when(doubleGenre.toString()).thenReturn("Fantasia");
+
+            Description mockDescription = mockedDescription.constructed().getFirst();
+            when(mockDescription.toString()).thenReturn(expectedDescription);
+
+            when(doubleRequest.getAuthorObjList()).thenReturn(null);
+
+            book.applyPatch(1L, doubleRequest);
+
+            verify(doubleRequest).getTitle();
+            verify(doubleRequest).getDescription();
+            verify(doubleRequest).getGenreObj();
+            verify(doubleRequest).getAuthorObjList();
+            verify(doubleRequest).getPhotoURI();
+
+            assertEquals(doubleGenre, book.getGenre());
+            assertEquals(expectedIsbn, book.getIsbn());
+            assertEquals(expectedTitle, book.getTitle().toString());
+            assertEquals(expectedAuthorList, book.getAuthors());
+            assertEquals(expectedDescription, book.getDescription());
+            assertEquals(expectedPhotoUri, book.getPhoto().getPhotoFile());
         }
     }
 }
